5ff58599dd3c0fb1c1ad444109b86edf016c7682
Kohei Watanabe <xbeef29@gmail.com>
2017-05-23 21:12:03 +0900

    macro re-export (copied from karabiner-thunk)

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,14 @@
 pub extern crate karabiner_justonce as justonce;
 pub extern crate karabiner_thunk as thunk;
+
+// macros cannot reexoprt.
+
+#[macro_export]
+macro_rules! lazy {
+    ( $e:expr ) => { $crate::thunk::Thunk::lazy(move || { $e }) };
+}
+
+#[macro_export]
+macro_rules! eval {
+    ( $e:expr ) => { $crate::thunk::Thunk::eval($e) };
+}
